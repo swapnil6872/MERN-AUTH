ae6f477ee5ba7e6ad08cfc0c60e59aea26ddc63f
Swapnil Gamit <gamitswapnil7@gmail.com>
2026-08-13 21:38:50 +0530

    basic configration done for react

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div>
+        <h1 class="text-3xl font-bold underline">
+           Hello world!
+        </h1>
+    </div>
+  )
+}
+
+export default App
