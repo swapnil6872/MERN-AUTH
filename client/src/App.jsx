816595348d89ed64567980ea7a5b1314d5a65ac6
Swapnil Gamit <gamitswapnil7@gmail.com>
2026-08-13 22:17:25 +0530

    homepage completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Routes ,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import EmailVerify from './pages/EmailVerify'
+import ResetPassword from './pages/ResetPassword'
 
 export const App = () => {
   return (
     <div>
-        <h1 class="text-3xl font-bold underline">
-           Hello world!
-        </h1>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/email-verify' element={<EmailVerify/>} />
+        <Route path='/Reset-password' element={<ResetPassword/>} />
+      </Routes>
+
     </div>
   )
 }
