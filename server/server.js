164a8e5c3238235b43f05367f3ac33947c30f4ff
Swapnil Gamit <gamitswapnil7@gmail.com>
2026-08-14 12:29:20 +0530

    added client url

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173',process.env.CLIENT_URL]
 
 app.use(express.json());
 app.use(cookieParser());
